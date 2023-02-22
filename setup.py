418907d711995dfa90db62e354a7d39a431f867a
GBJ <gbialystokjoly@gmail.com>
2023-02-22 09:42:05 +0100

    Had to change name to easyflask.

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
-from setuptools import setup
 
+from distutils.core import setup
 setup(
-   name='ezFlask',
-   version='0.1.0',
-   author='WhMonkey',
-   packages=['ezFlask'],
-   license='LICENSE.txt',
-   description='An easy to use flask wrapper.',
-   long_description=open('README.md').read(),
-   install_requires=[
-       "flask",
-       "flask_cors",
-   ],
+  name = 'easyflask',        
+  packages = ['easyflask'],   
+  version = '0.1',      
+  license='MIT',        
+  description = 'An easier and more simple version of flask.',
+  author = 'WhMonkey',                   
+  author_email = 'no-reply@no-reply.com',      
+  url = 'https://github.com/whineymonkey10/ezflask',   
+  download_url = 'https://github.com/WhineyMonkey10/EzFlask/archive/refs/tags/v.0.1.0.tar.gz',    
+  keywords = ['FLASK', 'WEBSITE', 'EASY'],   
+  install_requires=[            
+          'flask',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3',     
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
 )
